f0ab4cb920478084fb434ba9687fddc193c703d5
bluepearl2018 <36004318+bluepearl2018@users.noreply.github.com>
2019-04-03 13:54:48 +0100

    Laravel passport vues integrated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,8 +13,9 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
+                    <passport-clients></passport-clients>
+                    <passport-authorized-clients></passport-authorized-clients>
+                    <passport-personal-access-tokens-clients></passport-personal-access-tokens-clients>
                 </div>
             </div>
         </div>
